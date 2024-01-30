3738016880e292ef4e2af3d7b768f894a331417f
Your Name <cwalczak2>
2024-01-30 16:28:51 -0500

    random string generated

--- hello.py
+++ hello.py
@@ -1,3 +1,23 @@
-print("Hello World, this is my frist py project")
+import random
 
-print("some extra stuff")
+def generatePassword(len):
+    password = ""
+    for i in range(len):
+        ascii_val = random.randint(32, 126)
+        character = chr(ascii_val)
+        password+=character
+    return password 
+
+def main():
+    len = 16
+
+    newPassword = generatePassword(len)
+    print(f"Generated Pasword: {newPassword}")
+
+    
+if __name__ == "__main__":
+    # Call the main function when the script is executed
+    main()
+
+
+
